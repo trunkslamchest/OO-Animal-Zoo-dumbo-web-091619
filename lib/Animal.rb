07f3a47412fe109be92e9894bfc69a97d12e7469
Austin Smith <trunk9slamchest@gmail.com>
2019-09-19 17:59:09 +0000

    Automatically backed up by Learn

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,28 @@
 class Animal
 
+	attr_reader :nickname, :species, :zoo
+	attr_accessor :weight
+
+	@@all = []
+
+	def initialize(nickname, species, weight, zoo)
+		@nickname = nickname
+		@species = species
+		@weight = weight
+		@zoo = zoo
+		@@all << self
+	end
+
+	# def zoo
+	# 	Zoo.all.select { |zoos| zoos.location == self }
+	# end
+
+	def self.find_by_species(speci)
+		Animal.all.select { |animals| animals.species == speci }
+	end
+
+	def self.all
+		@@all
+	end
+
 end
